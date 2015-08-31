8fdc9a04f362828c8de13a1358efed6ce6912bfa
Semen Grigorev <ftip@list.ru>
2015-08-31 19:23:53 +0300

    Lesson#4 drawer

--- app/src/main/java/com/example/ftipip/moneytrackerv/MainActivity.java
+++ app/src/main/java/com/example/ftipip/moneytrackerv/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.ftipip.moneytrackerv;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-
+import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private View container;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,17 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
 
+    private void setNavigationDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener((new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                return false;
+            }
+        }));
     }
 
 
