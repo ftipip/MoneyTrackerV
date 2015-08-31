7d11112015cf0a7f3be2177ae5278f2c8f84897e
Semen Grigorev <ftip@list.ru>
2015-08-31 19:00:35 +0300

    Lesson#4 fragment

--- app/src/main/java/com/example/ftipip/moneytrackerv/MainActivity.java
+++ app/src/main/java/com/example/ftipip/moneytrackerv/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.ftipip.moneytrackerv;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -20,6 +20,18 @@ public class MainActivity extends ActionBarActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new ExpensesFragment()).commit();}
 
         Log.d(LOG_TAG, "onCreate");
+    }
+
+    private void initToolbar() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 
 
