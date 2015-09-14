eb045f738568da05b08e860f1c76aba6bfecccd8
Semen Grigorev <ftip@list.ru>
2015-09-14 17:30:56 +0300

    Lesson#6DBFlow

--- app/src/main/java/com/example/ftipip/moneytrackerv/MoneyTrackerApplication.java
+++ app/src/main/java/com/example/ftipip/moneytrackerv/MoneyTrackerApplication.java
@@ -2,15 +2,16 @@ package com.example.ftipip.moneytrackerv;
 
 import android.app.Application;
 
+import com.activeandroid.ActiveAndroid;
 import com.raizlabs.android.dbflow.config.FlowManager;
 
 /**
  * Created by ftipip on 11.09.2015.
  */
-public class MoneyTrackerApplication extends Application {
+public class MoneyTrackerApplication extends com.activeandroid.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FlowManager.init(this);
+        ActiveAndroid.initialize(this);
     }
 }
